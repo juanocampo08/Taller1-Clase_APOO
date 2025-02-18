792d8c9e7dd940b2c70cfe4f9ad9616b4b625286
JuanOcampo <juanjocampogomrz13@gmail.com>
2025-02-18 11:04:19 -0500

    se crearon las variables constantes y sus respectivos atributos en el metodo init de la clase note

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,17 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from datetime import datetime
+
+class Note:
+    HIGH: str = 'HIGH'
+    MEDIUM: str = 'MEDIUM'
+    LOW: str = 'LOW'
+
+    def __init__(self, code: str, title: str, text: str, importance: str ):
+        self.code = code
+        self.title = title
+        self.text = text
+        self.importance = importance
+        self.creation_date = datetime.now()
+        self.tags: list[str] = []
+
+
